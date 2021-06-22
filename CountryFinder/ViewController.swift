7f3dd377e0b83e5065977bfa67d098cbe96213ff
Daniel Duran Schutz <danissch@gmail.com>
2021-06-22 10:26:32 -0500

    Initial Commit

---
+++ CountryFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountryFinder
+//
+//  Created by Daniel Duran Schutz on 22/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
